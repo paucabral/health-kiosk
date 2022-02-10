5ecea1ba55c862f5e52efd056b852c9f01f48ec2
paucabral <josepaucabral@gmail.com>
2022-02-10 13:49:20 +0800

    Added hooks for paging on form fill up

--- frontend/src/components/Triage.js
+++ frontend/src/components/Triage.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardHeader, MDBCardFooter, MDBBtn, MDBContainer, MDBProgress, MDBProgressBar } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardHeader, MDBCardFooter, MDBCol, MDBRow, MDBBtn, MDBContainer, MDBProgress, MDBProgressBar } from 'mdb-react-ui-kit';
 import PersonalInformationInput from './triage_forms/PersonalInformationInput';
 
 const Triage = () => {
+  const [page, setPage] = useState(0);
   const [progress, setProgress] = useState(0);
+  const FormTitles = ['Personal Information', 'Next']
 
   return (
     <React.Fragment>
@@ -11,6 +13,15 @@ const Triage = () => {
         <MDBCard className='p-5'>
 
           <PersonalInformationInput />
+
+          <MDBRow className='mb-4 w-100'>
+            <MDBCol>
+              <MDBBtn className='w-50' disabled={page == 0} onClick={() => setPage((currPage) => currPage - 1)}>Prev</MDBBtn>
+            </MDBCol>
+            <MDBCol>
+              <MDBBtn className='w-50' disabled={page == FormTitles.length - 1} onClick={() => setPage((currPage) => currPage + 1)}>Next</MDBBtn>
+            </MDBCol>
+          </MDBRow>
 
         </MDBCard>
         <MDBProgress className='m-3'>
