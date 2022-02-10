9ae4917ad5c39d25f3b648e8c88754760c655a8a
paucabral <josepaucabral@gmail.com>
2022-02-10 18:27:34 +0800

    Added button to call temperature measurement API.

--- frontend/src/components/triage_forms/sensor_modals/TemperatureModal.js
+++ frontend/src/components/triage_forms/sensor_modals/TemperatureModal.js
@@ -21,12 +21,17 @@ const TemperatureModal = ({ temperatureModal, setTemperatureModal, toggleTempera
               <MDBBtn className='btn-close' color='none' onClick={toggleTemperatureModal}></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody style={{ textAlign: "left" }}>
-              <p>Please follow these steps to measure your body temperature.</p>
-              <ol>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                <li>Aliquam interdum risus sit amet urna lacinia, sit amet efficitur augue bibendum.</li>
-                <li>Phasellus non ex condimentum, accumsan justo quis, molestie neque.</li>
-              </ol>
+              <div>
+                <p>Please follow these steps to measure your body temperature.</p>
+                <ol>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                  <li>Aliquam interdum risus sit amet urna lacinia, sit amet efficitur augue bibendum.</li>
+                  <li>Phasellus non ex condimentum, accumsan justo quis, molestie neque.</li>
+                </ol>
+              </div>
+              <div className='d-flex align-items-center justify-content-center'>
+                <MDBBtn>Start Measurement</MDBBtn>
+              </div>
             </MDBModalBody>
             <MDBModalFooter>
               <MDBBtn color='danger' onClick={toggleTemperatureModal}>
