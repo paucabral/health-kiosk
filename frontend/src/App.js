9beeb871821b69724472bff0f81440e0df5fddd5
paucabral <josepaucabral@gmail.com>
2022-03-02 16:54:12 +0800

    Layout structure'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navigation from './components/Navigation'
 import Triage from './components/Triage';
 import { MDBContainer } from 'mdb-react-ui-kit';
+import Facilities from './Facilities';
 
 function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <Navigation />
       <div className='d-flex align-items-center justify-content-center' style={{ height: "90vh" }}>
-        <Triage />
+        {/* <Triage /> */}
+        <Facilities />
       </div>
     </div>
   );
--- frontend/src/Facilities.js
+++ frontend/src/Facilities.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBCardSubTitle, MDBCardText } from 'mdb-react-ui-kit';
 
 const Facilities = () => {
   return (
     <React.Fragment>
-      Facilities
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md='4'>
+            <MDBRow>
+              <MDBCard>
+                <MDBCardBody className='text-left'>
+                  <MDBCardTitle>Current Location</MDBCardTitle>
+                  <MDBCardSubTitle className='text-muted'>Coordinates</MDBCardSubTitle>
+                  <MDBCardText>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBRow>
+          </MDBCol>
+          <MDBCol md='8'>
+            <MDBCard style={{ width: '100%', height: '100%' }}>
+
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </React.Fragment>
   )
 }
