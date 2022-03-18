444a52afa4c94f473ed9f6a4dc3e7dd9febed056
paucabral <josepaucabral@gmail.com>
2022-03-18 23:38:49 +0800

    feat: enclose vital sign confirmation grids into cards

--- frontend/src/components/triage_forms/Confirmation.js
+++ frontend/src/components/triage_forms/Confirmation.js
@@ -1,4 +1,4 @@
-import { MDBCardBody, MDBIcon, MDBCol, MDBRow, MDBCardTitle, MDBBtn, MDBContainer } from 'mdb-react-ui-kit'
+import { MDBCard, MDBCardHeader, MDBCardTitle, MDBCardText, MDBCardBody, MDBIcon, MDBCol, MDBRow, MDBBtn, MDBContainer } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const Confirmation = ({ formData, setFormData, page, setPage }) => {
@@ -63,44 +63,64 @@ const Confirmation = ({ formData, setFormData, page, setPage }) => {
           </MDBRow>
           <MDBRow className='mb-4' style={{ textAlign: "center" }}>
             <MDBCol>
-              <MDBRow>
-                <strong><MDBIcon fas icon="thermometer-quarter" /> Temperature:</strong>
-              </MDBRow>
-              <MDBRow>
-                <span>{formData.temperature} °C</span>
-              </MDBRow>
+              <MDBCard shadow='0' border='warning' background='white'>
+                <MDBCardHeader className='text-warning'>
+                  <strong><MDBIcon fas icon="thermometer-quarter" /> Body Temperature</strong>
+                </MDBCardHeader>
+                <MDBCardBody className='text-warning'>
+                  <MDBCardText>
+                    <span>{formData.temperature} °C</span>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
             </MDBCol>
             <MDBCol>
-              <MDBRow>
-                <strong><MDBIcon fas icon="wind" /> O<sub>2</sub> Saturation:</strong>
-              </MDBRow>
-              <MDBRow>
-                <span> {formData.o2_saturation} %</span>
-              </MDBRow>
+              <MDBCard shadow='0' border='success' background='white'>
+                <MDBCardHeader className='text-success'>
+                  <strong><MDBIcon fas icon="wind" /> O<sub>2</sub> Saturation</strong>
+                </MDBCardHeader>
+                <MDBCardBody className='text-success'>
+                  <MDBCardText>
+                    <span> {formData.o2_saturation} %</span>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
             </MDBCol>
             <MDBCol>
-              <MDBRow>
-                <strong><MDBIcon fas icon="heartbeat" /> Pulse Rate:</strong>
-              </MDBRow>
-              <MDBRow>
-                <span>{formData.pulse_rate} bpm</span>
-              </MDBRow>
+              <MDBCard shadow='0' border='danger' background='white'>
+                <MDBCardHeader className='text-danger'>
+                  <strong><MDBIcon fas icon="heartbeat" /> Pulse Rate</strong>
+                </MDBCardHeader>
+                <MDBCardBody className='text-danger'>
+                  <MDBCardText>
+                    <span>{formData.pulse_rate} bpm</span>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
             </MDBCol>
             <MDBCol>
-              <MDBRow>
-                <strong><MDBIcon fas icon="tint" /> Systolic BP:</strong>
-              </MDBRow>
-              <MDBRow>
-                <span>{formData.systolic_bp} mmHg</span>
-              </MDBRow>
+              <MDBCard shadow='0' border='primary' background='white'>
+                <MDBCardHeader className='text-primary'>
+                  <strong><MDBIcon fas icon="tint" /> Systolic BP</strong>
+                </MDBCardHeader>
+                <MDBCardBody className='text-primary'>
+                  <MDBCardText>
+                    <span>{formData.systolic_bp} mmHg</span>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
             </MDBCol>
             <MDBCol>
-              <MDBRow>
-                <strong><MDBIcon fas icon="tint" />Diastolic BP:</strong>
-              </MDBRow>
-              <MDBRow>
-                <span>{formData.diastolic_bp} mmHg</span>
-              </MDBRow>
+              <MDBCard shadow='0' border='secondary' background='white'>
+                <MDBCardHeader className='text-secondary'>
+                  <strong><MDBIcon fas icon="tint" /> Diastolic BP</strong>
+                </MDBCardHeader>
+                <MDBCardBody className='text-secondary'>
+                  <MDBCardText>
+                    <span>{formData.diastolic_bp} mmHg</span>
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
