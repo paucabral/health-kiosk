d51fd170fca96b59592b7dbec0e3839c3b454042
paucabral <josepaucabral@gmail.com>
2022-03-18 23:09:40 +0800

    feat: change personal entry confirm to grid

--- frontend/src/components/triage_forms/Confirmation.js
+++ frontend/src/components/triage_forms/Confirmation.js
@@ -1,4 +1,4 @@
-import { MDBCardBody, MDBIcon, MDBCol, MDBRow, MDBCardTitle, MDBBtn } from 'mdb-react-ui-kit'
+import { MDBCardBody, MDBIcon, MDBCol, MDBRow, MDBCardTitle, MDBBtn, MDBContainer } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const Confirmation = ({ formData, setFormData, page, setPage }) => {
@@ -13,21 +13,71 @@ const Confirmation = ({ formData, setFormData, page, setPage }) => {
         <div className='mb-4'>
           Please ensure all the information are correct before you proceed.
         </div>
-        <MDBRow>
-          <MDBCol className='mb-4' style={{ textAlign: "left" }}>
-            <span><strong>Name:</strong> {formData.first_name} {formData.last_name}</span><br />
-            <span><strong>Sex:</strong> {formData.sex}</span><br />
-            <span><strong>Age:</strong> {formData.age}</span><br />
-            <span><strong>Contact No:</strong> {formData.contact_no}</span><br />
-          </MDBCol>
-          <MDBCol className='mb-4' style={{ textAlign: "left" }}>
-            <span><strong><MDBIcon fas icon="thermometer-quarter" /> Body Temperature:</strong> {formData.temperature} °C</span><br />
-            <span><strong><MDBIcon fas icon="heartbeat" /> Pulse Rate:</strong> {formData.pulse_rate} bpm</span><br />
-            <span><strong><MDBIcon fas icon="tint" /> Systolic BP:</strong> {formData.systolic_bp} mmHg</span><br />
-            <span><strong><MDBIcon fas icon="tint" /> Diastolic BP:</strong> {formData.diastolic_bp} mmHg</span><br />
-            <span><strong><MDBIcon fas icon="wind" /> O2 Saturation:</strong> {formData.o2_saturation} %</span><br />
-          </MDBCol>
-        </MDBRow>
+        <MDBContainer>
+          <MDBRow className='mb-4' style={{ textAlign: "left", display: 'flex', justifyContent: "center" }}>
+            <MDBRow>
+              <MDBCol>
+                <MDBRow>
+                  <MDBCol size='3'>
+                    <strong>NAME:</strong>
+                  </MDBCol>
+                  <MDBCol>
+                    <span>{formData.first_name} {formData.last_name}</span>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCol>
+              <MDBCol>
+                <MDBRow>
+                  <MDBCol size='3'>
+                    <strong>SEX:</strong>
+                  </MDBCol>
+                  <MDBCol>
+                    <span>{formData.sex}</span>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol>
+                <MDBRow>
+                  <MDBCol size='3'>
+                    <strong>AGE:</strong>
+                  </MDBCol>
+                  <MDBCol>
+                    <span>{formData.age}</span>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCol>
+              <MDBCol>
+                <MDBRow>
+                  <MDBCol size='3'>
+                    <strong>CONTACT:</strong>
+                  </MDBCol>
+                  <MDBCol>
+                    <span>{formData.contact_no}</span>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCol>
+            </MDBRow>
+          </MDBRow>
+          <MDBRow className='mb-4' style={{ textAlign: "center" }}>
+            <MDBCol>
+              <span><strong><MDBIcon fas icon="thermometer-quarter" /> Body Temperature:</strong> {formData.temperature} °C</span>
+            </MDBCol>
+            <MDBCol>
+              <span><strong><MDBIcon fas icon="wind" /> O2 Saturation:</strong> {formData.o2_saturation} %</span>
+            </MDBCol>
+            <MDBCol>
+              <span><strong><MDBIcon fas icon="heartbeat" /> Pulse Rate:</strong> {formData.pulse_rate} bpm</span>
+            </MDBCol>
+            <MDBCol>
+              <span><strong><MDBIcon fas icon="tint" /> Systolic BP:</strong> {formData.systolic_bp} mmHg</span>
+            </MDBCol>
+            <MDBCol>
+              <span><strong><MDBIcon fas icon="tint" /> Diastolic BP:</strong> {formData.diastolic_bp} mmHg</span>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
         <div>
           <MDBBtn onClick={() => { setPage(0) }} className='mx-2'>
             Go Back
