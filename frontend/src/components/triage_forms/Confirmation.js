9af2f13ae698e1e6f4140d77a42f8448a66e2cb3
paucabral <josepaucabral@gmail.com>
2022-03-18 23:53:27 +0800

    feat: add scrollable box for entry confirmation.

--- frontend/src/components/triage_forms/Confirmation.js
+++ frontend/src/components/triage_forms/Confirmation.js
@@ -8,133 +8,137 @@ const Confirmation = ({ formData, setFormData, page, setPage }) => {
 
   return (
     <React.Fragment>
-      <MDBCardTitle style={{ fontWeight: "bold" }}>Confirm Entry</MDBCardTitle>
-      <MDBCardBody>
-        <MDBContainer className='mb-4'>
-          Please ensure all the information are correct before you proceed.
-        </MDBContainer>
+      <div style={{ height: '67vh' }}>
+        <MDBCardTitle style={{ fontWeight: "bold" }}>Confirm Entry</MDBCardTitle>
+        <MDBCardBody>
+          <MDBContainer className='mb-2'>
+            Please ensure that all the information are correct before you proceed.
+          </MDBContainer>
 
-        <MDBContainer>
-          <MDBRow className='mb-4' style={{ textAlign: "left", display: 'flex', justifyContent: "center" }}>
-            <MDBRow>
-              <MDBCol>
+          <div style={{ overflowX: 'scroll', height: '40vh', marginTop: '1em', marginBottom: '1em' }}>
+            <div className='container'>
+              <MDBRow className='mb-3' style={{ textAlign: "left", display: 'flex', justifyContent: "center" }}>
                 <MDBRow>
-                  <MDBCol size='3'>
-                    <strong>NAME:</strong>
+                  <MDBCol>
+                    <MDBRow>
+                      <MDBCol size='3'>
+                        <strong>NAME:</strong>
+                      </MDBCol>
+                      <MDBCol>
+                        <span>{formData.first_name} {formData.last_name}</span>
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                   <MDBCol>
-                    <span>{formData.first_name} {formData.last_name}</span>
+                    <MDBRow>
+                      <MDBCol size='3'>
+                        <strong>SEX:</strong>
+                      </MDBCol>
+                      <MDBCol>
+                        <span>{formData.sex}</span>
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
-              </MDBCol>
-              <MDBCol>
                 <MDBRow>
-                  <MDBCol size='3'>
-                    <strong>SEX:</strong>
+                  <MDBCol>
+                    <MDBRow>
+                      <MDBCol size='3'>
+                        <strong>AGE:</strong>
+                      </MDBCol>
+                      <MDBCol>
+                        <span>{formData.age}</span>
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                   <MDBCol>
-                    <span>{formData.sex}</span>
+                    <MDBRow>
+                      <MDBCol size='3'>
+                        <strong>CONTACT:</strong>
+                      </MDBCol>
+                      <MDBCol>
+                        <span>{formData.contact_no}</span>
+                      </MDBCol>
+                    </MDBRow>
                   </MDBCol>
                 </MDBRow>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol>
-                <MDBRow>
-                  <MDBCol size='3'>
-                    <strong>AGE:</strong>
-                  </MDBCol>
-                  <MDBCol>
-                    <span>{formData.age}</span>
-                  </MDBCol>
-                </MDBRow>
-              </MDBCol>
-              <MDBCol>
-                <MDBRow>
-                  <MDBCol size='3'>
-                    <strong>CONTACT:</strong>
-                  </MDBCol>
-                  <MDBCol>
-                    <span>{formData.contact_no}</span>
-                  </MDBCol>
-                </MDBRow>
-              </MDBCol>
-            </MDBRow>
-          </MDBRow>
-          <MDBRow className='mb-4' style={{ textAlign: "center" }}>
-            <MDBCol>
-              <MDBCard shadow='0' border='warning' background='white'>
-                <MDBCardHeader className='text-warning'>
-                  <strong><MDBIcon fas icon="thermometer-quarter" /> Body Temperature</strong>
-                </MDBCardHeader>
-                <MDBCardBody className='text-warning'>
-                  <MDBCardText>
-                    <span>{formData.temperature} °C</span>
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard shadow='0' border='success' background='white'>
-                <MDBCardHeader className='text-success'>
-                  <strong><MDBIcon fas icon="wind" /> O<sub>2</sub> Saturation</strong>
-                </MDBCardHeader>
-                <MDBCardBody className='text-success'>
-                  <MDBCardText>
-                    <span> {formData.o2_saturation} %</span>
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard shadow='0' border='danger' background='white'>
-                <MDBCardHeader className='text-danger'>
-                  <strong><MDBIcon fas icon="heartbeat" /> Pulse Rate</strong>
-                </MDBCardHeader>
-                <MDBCardBody className='text-danger'>
-                  <MDBCardText>
-                    <span>{formData.pulse_rate} bpm</span>
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard shadow='0' border='primary' background='white'>
-                <MDBCardHeader className='text-primary'>
-                  <strong><MDBIcon fas icon="tint" /> Systolic BP</strong>
-                </MDBCardHeader>
-                <MDBCardBody className='text-primary'>
-                  <MDBCardText>
-                    <span>{formData.systolic_bp} mmHg</span>
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-            <MDBCol>
-              <MDBCard shadow='0' border='secondary' background='white'>
-                <MDBCardHeader className='text-secondary'>
-                  <strong><MDBIcon fas icon="tint" /> Diastolic BP</strong>
-                </MDBCardHeader>
-                <MDBCardBody className='text-secondary'>
-                  <MDBCardText>
-                    <span>{formData.diastolic_bp} mmHg</span>
-                  </MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
+              </MDBRow>
+              <MDBRow className='mb-4' style={{ textAlign: "center" }}>
+                <MDBCol>
+                  <MDBCard shadow='0' border='warning' background='white'>
+                    <MDBCardHeader className='text-warning'>
+                      <strong><MDBIcon fas icon="thermometer-quarter" /> Body Temperature</strong>
+                    </MDBCardHeader>
+                    <MDBCardBody className='text-warning'>
+                      <MDBCardText>
+                        <span>{formData.temperature} °C</span>
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                  <MDBCard shadow='0' border='success' background='white'>
+                    <MDBCardHeader className='text-success'>
+                      <strong><MDBIcon fas icon="wind" /> O<sub>2</sub> Saturation</strong>
+                    </MDBCardHeader>
+                    <MDBCardBody className='text-success'>
+                      <MDBCardText>
+                        <span> {formData.o2_saturation} %</span>
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                  <MDBCard shadow='0' border='danger' background='white'>
+                    <MDBCardHeader className='text-danger'>
+                      <strong><MDBIcon fas icon="heartbeat" /> Pulse Rate</strong>
+                    </MDBCardHeader>
+                    <MDBCardBody className='text-danger'>
+                      <MDBCardText>
+                        <span>{formData.pulse_rate} bpm</span>
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                  <MDBCard shadow='0' border='primary' background='white'>
+                    <MDBCardHeader className='text-primary'>
+                      <strong><MDBIcon fas icon="tint" /> Systolic BP</strong>
+                    </MDBCardHeader>
+                    <MDBCardBody className='text-primary'>
+                      <MDBCardText>
+                        <span>{formData.systolic_bp} mmHg</span>
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                  <MDBCard shadow='0' border='secondary' background='white'>
+                    <MDBCardHeader className='text-secondary'>
+                      <strong><MDBIcon fas icon="tint" /> Diastolic BP</strong>
+                    </MDBCardHeader>
+                    <MDBCardBody className='text-secondary'>
+                      <MDBCardText>
+                        <span>{formData.diastolic_bp} mmHg</span>
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </MDBCol>
+              </MDBRow>
+            </div>
+          </div>
 
-        <MDBContainer>
-          <MDBBtn onClick={() => { setPage(0) }} className='mx-2'>
-            Go Back
-          </MDBBtn>
-          <MDBBtn color='success' disabled={checkEntry()}>
-            Submit
-          </MDBBtn>
-        </MDBContainer>
+          <MDBContainer>
+            <MDBBtn onClick={() => { setPage(0) }} className='mx-2'>
+              Go Back
+            </MDBBtn>
+            <MDBBtn color='success' disabled={checkEntry()}>
+              Submit
+            </MDBBtn>
+          </MDBContainer>
 
-      </MDBCardBody>
+        </MDBCardBody>
+      </div>
     </React.Fragment>
   )
 }
