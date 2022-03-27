8532eb795bc27effdd7e395e38745596923e0536
paucabral <josepaucabral@gmail.com>
2022-03-27 23:24:53 +0800

    feat: add sample Python file for getting predictions.

--- machine_learning/get_predictions.py
+++ machine_learning/get_predictions.py
@@ -1,43 +1,26 @@
-import findspark
-findspark.init()
-from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark.sql import SparkSession
-# import findspark
-# findspark.init()
+import pandas as pd
 
-spark = SparkSession.builder.appName(
-    'DifferentialDiagnosisSystem').getOrCreate()
+# Loading the generated prediction results from the model.
+RECOMMENDATIONS_PATH = './result/recommendations_df.csv'
+recommendations_df = pd.read_csv(RECOMMENDATIONS_PATH)
 
-MODEL_PATH = './model/model_recommender'
-model = ALSModel.load(MODEL_PATH)
+# Sample list of symptom names.
+symptoms_list = ['pain chest', 'nausea', 'sweat', 'fall', 'angina pectoris']
 
-SPARSE_MATRIX_CSV = './dataset/symptoms-disease-weighted-rank-count_sparse-matrix.csv'
-SYMPTOMS_CSV = './dataset/symptoms_id.csv'
-DIAGNOSIS_CSV = './dataset/diseases_id.csv'
+# Finding entries where entries from the symptoms list appears.
+results = recommendations_df[recommendations_df['symptom'].isin(
+    symptoms_list)].sort_values(by=['symptom'], ascending=False)
 
-diff = spark.read.csv(SPARSE_MATRIX_CSV, header=True, inferSchema=True)
-sym = spark.read.csv(SYMPTOMS_CSV, header=True, inferSchema=True)
-dia = spark.read.csv(DIAGNOSIS_CSV, header=True, inferSchema=True)
+# Narrowing the data frame search results to disease and prediction columns and summing the prediction values of same disease names.
+result_df = recommendations_df[['disease', 'prediction']]
+df = result_df.groupby('disease')[
+    'prediction'].sum().sort_values(ascending=False)
 
-df = diff.join(sym, ['syd'], how='inner')
-df = df.join(dia, ['did'], how='inner')
-# df.show(4)
-df_pandasDf = df.toPandas()
+# Previewing the series data.
+print(df)
 
-# Prediction
-print('Symptoms: pain chest, palpitation, shortness of breath, sweat')
-sydId1 = 'pain chest'
-sydId2 = 'palpitation'
-sydId3 = 'shortness of breath'
-sydId4 = 'sweat'
-df1a = df.filter((df['symptom'] == sydId1) | (df['symptom'] == sydId2) |
-                 (df['symptom'] == sydId3) | (df['symptom'] == sydId4)).select('syd', 'symptom', 'did', 'disease', 'wei').orderBy('wei', ascending=False)
-df1a.show()
+# Converting the data to dictionary.
+output = df.to_dict()
 
-# recommendations = model.transform(df1a).orderBy('prediction', ascending=False)
-# print('Recommendations: ')
-# recommendations_df = recommendations.toPandas()
-# print(recommendations_df.head())
-
-# model.recommendForAllUsers(10).show()
-model.transform(df).show()
+# Preview for the output.
+print(output)
