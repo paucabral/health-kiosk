e19388584ba06efb3bec62f61f9dad33ecc17148
paucabral <josepaucabral@gmail.com>
2022-03-28 00:44:40 +0800

    fix: change dataframe variable

--- machine_learning/get_predictions.py
+++ machine_learning/get_predictions.py
@@ -12,7 +12,7 @@ results = recommendations_df[recommendations_df['symptom'].isin(
     symptoms_list)].sort_values(by=['symptom'], ascending=False)
 
 # Narrowing the data frame search results to disease and prediction columns and summing the prediction values of same disease names.
-result_df = recommendations_df[['disease', 'prediction']]
+result_df = results[['disease', 'prediction']]
 df = result_df.groupby('disease')[
     'prediction'].sum().sort_values(ascending=False)
 
