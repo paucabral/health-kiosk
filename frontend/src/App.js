463e767f9015370f011efdafdacdaec839961395
paucabral <josepaucabral@gmail.com>
2022-03-31 22:36:41 +0800

    feat: implement router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Triage from './screens/Triage';
-import { MDBContainer } from 'mdb-react-ui-kit';
 import Facilities from './screens/Facilities';
 
 function App() {
@@ -9,8 +9,13 @@ function App() {
     <div className="App" style={{ height: "95vh", width: "100vw" }}>
       <Navigation />
       <div className='d-flex align-items-center justify-content-center' style={{ height: "100%" }}>
-        <Triage />
-        {/* <Facilities /> */}
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Triage />} />
+            <Route exact path="/check" element={<Triage />} />
+            <Route path="/nearest-facilities" element={<Facilities />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
