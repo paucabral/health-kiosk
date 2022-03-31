b59348c974e21934baab650f43c1fe3b443856de
paucabral <josepaucabral@gmail.com>
2022-03-31 21:03:03 +0800

    feat: instantiate models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+  SEX_CHOICES = [
+    ('MALE', 'MALE'),
+    ('FEMALE', 'FEMALE')
+  ]
+
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  age = models.PositiveIntegerField()
+  sex = models.CharField(max_length=6, choices=SEX_CHOICES)
+  symptoms = models.JSONField()
+  contact_no = models.CharField(max_length=20)
+  temperature = models.FloatField(null=True)
+  pulse_rate = models.FloatField(null=True)
+  systolic_bp = models.FloatField(null=True)
+  diastolic_bp = models.FloatField(null=True)
+  o2_saturation = models.FloatField(null=True)
+  date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+  def __str__(self):
+    return "{} {}".format(self.first_name, self.last_name)
--- backend/health-kiosk/settings.py
+++ backend/health-kiosk/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
