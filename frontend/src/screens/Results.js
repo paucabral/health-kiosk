1b37bc87f188ee7ea9e8b89e378e66f3d3716657
paucabral <josepaucabral@gmail.com>
2022-04-02 22:42:45 +0800

    feat: patient info and symptoms on results page

--- frontend/src/screens/Results.js
+++ frontend/src/screens/Results.js
@@ -1,4 +1,4 @@
-import { MDBContainer } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBBtn } from 'mdb-react-ui-kit';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import VitalSigns from '../components/results/VitalSigns';
@@ -9,6 +9,71 @@ const Results = () => {
   return (
     <React.Fragment>
       <MDBContainer>
+        <MDBRow className='mb-3' style={{ textAlign: "center", display: 'flex', justifyContent: "center" }}>
+          <MDBCardTitle style={{ fontWeight: "bold" }}>Patient Assessment</MDBCardTitle>
+        </MDBRow>
+        <MDBRow className='mb-3' style={{ textAlign: "left", display: 'flex', justifyContent: "center" }}>
+          <MDBRow>
+            <MDBCol>
+              <MDBRow>
+                <MDBCol size='3'>
+                  <strong>NAME:</strong>
+                </MDBCol>
+                <MDBCol>
+                  {location.state.first_name && location.state.last_name ? <span>{location.state.first_name} {location.state.last_name}</span> : <i className='text-muted'>*MISSING</i>}
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+            <MDBCol>
+              <MDBRow>
+                <MDBCol size='3'>
+                  <strong>SEX:</strong>
+                </MDBCol>
+                <MDBCol>
+                  {location.state.sex ? <span>{location.state.sex}</span> : <i className='text-muted'>*MISSING</i>}
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol>
+              <MDBRow>
+                <MDBCol size='3'>
+                  <strong>AGE:</strong>
+                </MDBCol>
+                <MDBCol>
+                  {location.state.age ? <span>{location.state.age}</span> : <i className='text-muted'>*MISSING</i>}
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+            <MDBCol>
+              <MDBRow>
+                <MDBCol size='3'>
+                  <strong>CONTACT:</strong>
+                </MDBCol>
+                <MDBCol>
+                  {location.state.contact_no ? <span>{location.state.contact_no}</span> : <i className='text-muted'>*MISSING</i>}
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+          </MDBRow>
+          {
+            location.state.symptoms.length != 0 ?
+              <MDBRow className='mt-3 mb-2'>
+                <MDBCol size='2'>
+                  <strong>SYMPTOMS:</strong>
+                </MDBCol>
+                <MDBCol style={{ textAlign: 'left' }}>
+                  {location.state.symptoms?.map((symptom) => (
+                    <MDBBtn pill color='dark' className='shadow-0' key={symptom} style={{ display: 'inline', marginLeft: '0.3em', marginRight: '0.3em', marginBottom: '0.5em', borderRadius: '20px' }} disabled>{symptom}</MDBBtn>
+                  ))}
+                </MDBCol>
+              </MDBRow>
+              :
+              <></>
+          }
+        </MDBRow>
+
         {location.state.first_name} {location.state.last_name}
         <VitalSigns
           temperature={location.state.temperature}
