0de87d50751d11e0a4c509118126ca4f0b265c54
JoshuaLopez0378 <joshuaalbertlopez0378@gmail.com>
2022-04-05 03:09:26 +0800

    feat: test python script for sending hardcoded SMS

--- rpi/rpi_send_sms/test_sms.py
+++ rpi/rpi_send_sms/test_sms.py
@@ -1,1 +1,23 @@
 #Test confirmation comms
+import serial
+import os, time
+
+port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
+
+port.write("AT".encode())
+port.write('\r\n'.encode())
+rcv=port.read(10)
+print(rcv)
+
+port.write('AT+CMGS="+639709467719"'.encode())
+port.write('\r\n'.encode())
+rcv=port.read(10)
+print(rcv)
+
+port.write('Here is a test message from health kiosk'.encode())
+port.write('\r\n'.encode())
+port.write('\x1A'.encode())
+# port.write('\x1A'.encode())
+port.write('\r\n'.encode())
+rcv=port.read(10)
+print(rcv)
