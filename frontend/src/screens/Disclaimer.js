70ee36cc48251f6b1e44740931de230ef792cc60
paucabral <josepaucabral@gmail.com>
2022-04-05 04:03:28 +0800

    feat: modify disclaimer modal

--- frontend/src/screens/Disclaimer.js
+++ frontend/src/screens/Disclaimer.js
@@ -1,12 +1,17 @@
 import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit'
-import React from 'react'
+import React, { useState } from 'react'
+import TermsAndConditions from '../components/disclaimer/disclaimer_modal/TermsAndConditions';
 
 const Disclaimer = () => {
+  const [disclaimerModal, setDisclaimerModal] = useState(false);
+  const toggleDisclaimerModal = () => setDisclaimerModal(!disclaimerModal);
+
   return (
     <React.Fragment>
       <MDBContainer>
         <h1>Health Kiosk</h1>
-        <MDBBtn>Read Terms and Conditions</MDBBtn>
+        <MDBBtn onClick={toggleDisclaimerModal}>Read Terms and Conditions</MDBBtn>
+        <TermsAndConditions disclaimerModal={disclaimerModal} setDisclaimerModal={setDisclaimerModal} toggleDisclaimerModal={toggleDisclaimerModal} />
       </MDBContainer>
     </React.Fragment>
   )
