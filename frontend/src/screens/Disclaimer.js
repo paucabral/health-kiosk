5f3842c60d9d6687d6b05c7726489a29fcd2dd09
paucabral <josepaucabral@gmail.com>
2022-04-05 04:14:10 +0800

    feat: added boilerplate for disclaimer description

--- frontend/src/screens/Disclaimer.js
+++ frontend/src/screens/Disclaimer.js
@@ -10,6 +10,7 @@ const Disclaimer = () => {
     <React.Fragment>
       <MDBContainer>
         <h1>Health Kiosk</h1>
+        <p className='px-5' style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue nulla id lacinia blandit. Curabitur luctus est vitae semper fringilla. Proin dignissim ante eget orci aliquam, eget feugiat magna vestibulum. In at mauris nec purus commodo dignissim vitae et turpis. Vivamus venenatis dignissim ullamcorper. Aenean ut nibh ut nibh aliquam ultrices in vel quam. Pellentesque non ligula nec risus viverra tristique. Donec pretium lacus tempus orci cursus, non blandit purus tempus.</p>
         <MDBBtn onClick={toggleDisclaimerModal}>Read Terms and Conditions</MDBBtn>
         <TermsAndConditions disclaimerModal={disclaimerModal} setDisclaimerModal={setDisclaimerModal} toggleDisclaimerModal={toggleDisclaimerModal} />
       </MDBContainer>
