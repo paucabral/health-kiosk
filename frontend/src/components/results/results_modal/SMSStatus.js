5aa841f8117e208696a74b96e91fed875f54f9f2
paucabral <josepaucabral@gmail.com>
2022-04-05 16:06:20 +0800

    feat: add hooks for sending SMS

--- frontend/src/components/results/results_modal/SMSStatus.js
+++ frontend/src/components/results/results_modal/SMSStatus.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MDBBtn,
   MDBModal,
@@ -10,25 +10,55 @@ import {
   MDBModalFooter,
   MDBSpinner,
   MDBRow,
-  MDBCol
+  MDBCol,
+  MDBContainer,
+  MDBIcon
 } from 'mdb-react-ui-kit';
 
 const SMSStatus = ({ contact_no, message, smsModal, setSmsModal, toggleSmsModal }) => {
+  const [smsStatus, setSmsStatus] = useState("SENDING");
+
   return (
     <React.Fragment>
       <MDBModal id="disease-modal" staticBackdrop show={smsModal} setShow={setSmsModal} tabIndex='-1'>
         <MDBModalDialog centered size='lg'>
           <MDBModalContent style={{ height: '50vh' }}>
             <MDBModalHeader>
-              <MDBModalTitle>Obtain Results Through SMS</MDBModalTitle>
+              <MDBModalTitle>Send Results Through SMS</MDBModalTitle>
               <MDBBtn className='btn-close' color='none' onClick={toggleSmsModal}></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody className='my-2' style={{ height: '80vh' }}>
-              <MDBRow>
-                <p>Sending the assessment results through SMS to</p>
-                <h3>{contact_no}</h3>
-                <p className="text-muted" style={{ fontSize: '0.75em' }}><i>NOTE: Ensure that you have entered a working number.</i></p>
-              </MDBRow>
+              <MDBContainer>
+                {
+                  smsStatus === "SENDING" ?
+                    <div>
+                      <p>Sending the assessment results through SMS to</p>
+                      <h3>{contact_no}</h3>
+                      <MDBContainer>
+                        <MDBSpinner color='primary' />
+                      </MDBContainer>
+                      <p className="text-muted pt-3" style={{ fontSize: '0.75em' }}><i>NOTE: Ensure that the number you have entered is correct.</i></p>
+                    </div>
+                    : smsStatus === "SUCCESS" ?
+                      <div>
+                        <h3>The results has been sent successfully.</h3>
+                        <MDBContainer>
+                          <MDBIcon size='5x' color='success' fas icon="check-circle" />
+                        </MDBContainer>
+                        <p className="text-muted pt-3" style={{ fontSize: '0.75em' }}><i>NOTE: Ensure that the number you have entered is correct.</i></p>
+                      </div>
+                      :
+                      <div>
+                        <div>
+                          <h3>There was an error in sending the message.</h3>
+                          <MDBContainer>
+                            <MDBIcon size='5x' color='danger' fas icon="ban" />
+                          </MDBContainer>
+                          <p className="text-muted pt-3" style={{ fontSize: '0.75em' }}><i>NOTE: Ensure that the number you have entered is correct. Otherwise, please reach a support personnel.</i></p>
+                        </div>
+                      </div>
+                }
+              </MDBContainer>
             </MDBModalBody>
           </MDBModalContent>
         </MDBModalDialog>
