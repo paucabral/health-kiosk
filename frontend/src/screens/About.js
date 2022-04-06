6367c1a7433860f1d5a927919f60802dbd56b547
paucabral <josepaucabral@gmail.com>
2022-04-06 21:39:01 +0800

    feat: adjust text sizes and add button

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,20 +1,23 @@
-import { MDBContainer, MDBRow } from 'mdb-react-ui-kit'
+import { MDBBtn, MDBContainer, MDBRow } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const About = () => {
   return (
     <React.Fragment>
-      <MDBContainer>
+      <MDBContainer className='p-0 m-0'>
         <MDBRow id="banner">
           <MDBContainer className='mb-2'>
             <span style={{ fontSize: '0.75em' }}>BROUGHT TO YOU BY</span>
             <h5>HIGH GROUNDS</h5>
           </MDBContainer>
           <MDBContainer className='mb-4'>
-            <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>Health Kiosk</h1>
+            <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold', fontSize: '3em' }}>Health Kiosk</h1>
           </MDBContainer>
-          <MDBContainer style={{ width: '70vw' }}>
-            <p>A self-service health kiosk that performs differential diagnosis and provide recommendations for the user, as well as locate the nearby health facilities.</p>
+          <MDBContainer style={{ width: '65vw' }}>
+            <p style={{ fontSize: '1.25em' }}>A self-service health kiosk that performs differential diagnosis and provide recommendations for the user, as well as locate the nearby health facilities.</p>
+          </MDBContainer>
+          <MDBContainer className='my-3'>
+            <MDBBtn color='info'>Learn More</MDBBtn>
           </MDBContainer>
         </MDBRow>
         <MDBRow id="features">
