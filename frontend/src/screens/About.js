cb98a11d07ec1fbf70c878a4617c5686c038a359
paucabral <josepaucabral@gmail.com>
2022-04-06 21:34:27 +0800

    feat: instantiate banner section

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,11 +1,28 @@
-import { MDBContainer } from 'mdb-react-ui-kit'
+import { MDBContainer, MDBRow } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const About = () => {
   return (
     <React.Fragment>
       <MDBContainer>
-        Hello World
+        <MDBRow id="banner">
+          <MDBContainer className='mb-2'>
+            <span style={{ fontSize: '0.75em' }}>BROUGHT TO YOU BY</span>
+            <h5>HIGH GROUNDS</h5>
+          </MDBContainer>
+          <MDBContainer className='mb-4'>
+            <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>Health Kiosk</h1>
+          </MDBContainer>
+          <MDBContainer style={{ width: '70vw' }}>
+            <p>A self-service health kiosk that performs differential diagnosis and provide recommendations for the user, as well as locate the nearby health facilities.</p>
+          </MDBContainer>
+        </MDBRow>
+        <MDBRow id="features">
+
+        </MDBRow>
+        <MDBRow id="developers">
+
+        </MDBRow>
       </MDBContainer>
     </React.Fragment>
   )
