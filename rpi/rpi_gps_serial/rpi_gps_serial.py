7df1a7d6d60859ef9049760412d959c3f921fab8
JoshuaLopez0378 <joshuaalbertlopez0378@gmail.com>
2022-04-08 22:33:45 +0800

    feat: return list of dict instead of json, add variables to compensate for possible signal relock

--- rpi/rpi_gps_serial/rpi_gps_serial.py
+++ rpi/rpi_gps_serial/rpi_gps_serial.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import time
 import serial
-import json
 
 ser = serial.Serial(
     # port='/dev/ttyS0',
@@ -13,90 +12,85 @@ ser = serial.Serial(
     timeout=1
 )
 
+# For saving coords collected in case GPS signal fluctuates
+lng_save = 0.0
+lat_save = 0.0
 
 def compute_rawGPS(raw_val, direction):
     #Separate lat and long. Note: 2 digits to left of decimal point is min, rest is degree
-    str_int = str(int((raw_val//1))) #Converted to string, to separate degree and minutes
-    # str_flt = ("{:.4f}".format(raw_val%1)) # String as is
-    str_flt = float((raw_val%1)) # String as is
-
-    #Left hand side of decimal
-    deg = float(str_int[:-2]) #Degree is extracted, will be used as is
+    str_int = str(int((raw_val//1)))    #Converted to string, to separate degree and minutes
+    str_flt = float((raw_val%1))        #String as is
+                                        #Left hand side of decimal
+    deg = float(str_int[:-2])           #Degree is extracted, will be used as is
     min = float(str_int[-2:])
+                 
+    flt_val = float(str_flt)                        #Right hand side of decimal
+    full_minutes = min+flt_val                      #Combined
+    
+    coord_min = full_minutes/60.0                   #Take coordinate minute
+    coordinate_val = float(str(deg+coord_min)[:10]) #To shorten decimal places (string)
 
-    #Right hand side of decimal
-    flt_val = float(str_flt)
-
-    #combined
-    full_minutes = min+flt_val
-
-    #To take coordinate minute, refer to formula above
-    # coord_min = ("{:.4f}".format(full_minutes/60))
-    coord_min = full_minutes/60.0
-
-    # coordinate_val = float(str(deg+coord_min)[:8]) // to shorten decimal places (string)
-    coordinate_val = float(str(deg+coord_min)[:10])
-
-    if direction == "N" or direction == "E":
+    if direction == "N" or direction == "E":        #Condition to determine whether coord is + or -
         signed_coord = coordinate_val
-
     else:
         signed_coord = float(("-"+str(coordinate_val)))
         
-    if direction == "W" or direction == "E":
+    if direction == "W" or direction == "E":        #Just for print, can be removed
         latlong = "Long: "
-
     else:
         latlong = "Lat: "
+
     print(signed_coord)
     return signed_coord
 
-
-
-
 def computedGPS():
-    gpscnt = 0
-    strgpsarr = []
-    latGPRMC = []
+    gpscnt = 0                  # variable for iteration
+    strgpsarr = []              # array to hold all streams in string (for split function)
+
+    latGPRMC = []               # arrays for lat and long unused
     longGPRMC = []
-    latraw_val = 0.0
+    
+    latraw_val = 0.0            # initialize coord values and directions
     latdirection = ""
     longraw_val = 0.0
     longdirection = ""
-    gpsarr = []
-    gpslines=ser.readline()
-    strgpslines = str(gpslines).split(',')
-    gpsarr.append(strgpslines)
-    if len(gpsarr) < 1:
-        print("EMPTY")
+    
+    gpsarr = []                  # array to hold streams in b format   
+    gpslines=ser.readline()      # read from the port
+    strgpslines = str(gpslines).split(',')  # split by ,
+    gpsarr.append(strgpslines)     
 
-    else:
+    if len(gpsarr) < 1:          # initial check if something is read
+        print("EMPTY")            
+
+    else:                        # else proceed to check GPGLL
         if (gpsarr[0][0]) == "b'$GPGLL":
             print("FOUND GPGLL!")
+
             latraw_check = str(gpsarr[0][1]) # Checked initially, the firstmost value of needed
             if (latraw_check != ''):
-                latraw_val = float(gpsarr[0][1])
+                latraw_val = float(gpsarr[0][1])       # GPGLL pattern is $GPGLL, lat, lat_dir, lng, lng_dir
                 latdirection = str(gpsarr[0][2])
                 longraw_val = float(gpsarr[0][3])
                 longdirection = str(gpsarr[0][4])
-                # latraw_val = float(1437.57826)
-                # latdirection = str('N')
-                # longraw_val = float(12105.98656)
-                # longdirection = str('E')            
-                # print("RAW_Lat:",latraw_val)
-                # print("RAW_latdir:",latdirection)
-                # print("RAW_Long:",longraw_val)
-                # print("RAW_longdir:",longdirection)
-                lat = compute_rawGPS(latraw_val, latdirection)
-                lng = compute_rawGPS(longraw_val, longdirection)
-                coord_dic = {"lat": lat, "lng": lng}
-                jsonStr = json.dumps(coord_dic)
-                print(jsonStr)
+
+                lat = compute_rawGPS(latraw_val, latdirection)      # subject to computation
+                lng = compute_rawGPS(longraw_val, longdirection)      
+
+                lat_save = lat                                      # save values in case 
+                lng_save = lng
+
+                coord_pairs = [{"lat": lat}, {"lng": lng}]          # save as is
+
+                # print(coord_pairs)
             else:
-                print("GPS Not locked in yet")
-                
-    
-    
+                coord_pairs = [{"lat": lat_save}, {"lng": lng_save}] 
+                # print("Position untracked yet! But saved was: ", coord_pairs)
+            
+            print("Got",coord_pairs)
+            print("Saved",lat_save,lng_save)
+            return coord_pairs
+
     gpscnt+=1
     if gpscnt==8:
         gpscnt = 0
