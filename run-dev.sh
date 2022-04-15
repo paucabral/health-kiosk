b861cac0f3b21365f7e55c6a8477f078c6af6ae9
paucabral <josepaucabral@gmail.com>
2022-04-15 16:28:38 +0800

    feat: modify dev script to allow other lan devices

--- run-dev.sh
+++ run-dev.sh
@@ -7,7 +7,7 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver &
+python3 manage.py runserver 0.0.0.0:8000 &
 BACKEND_JOB=$!
 echo "$BACKEND_JOB"
 
