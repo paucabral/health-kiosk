e35ec263afca8891db5c320477eefc093ef03c63
paucabral <josepaucabral@gmail.com>
2022-04-19 14:45:06 +0800

    feat: improve sentences

--- frontend/src/screens/Disclaimer.js
+++ frontend/src/screens/Disclaimer.js
@@ -16,7 +16,7 @@ const Disclaimer = () => {
         <MDBContainer className='mb-4'>
           <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold', fontSize: '3em' }}>Health Kiosk</h1>
         </MDBContainer>
-        <p className='px-5' style={{ textAlign: 'center' }}><b>Disclaimer: </b>This kiosk was designed to aid the decision making of the patient and lead them proper channels for further consultation and analysis. The results are <b>NOT FINAL</b> <b>NOR A DEFINITIVE DIAGNOSIS</b>, but a indicator which can be used by a doctor to narrow down a patient's possible disease/s based on symptoms.</p>
+        <p className='px-5' style={{ textAlign: 'center' }}><b>Disclaimer: </b>This kiosk is designed to aid in the decision making of the patient and lead them proper channels for further consultation and analysis. The results are <b>NOT FINAL</b> <b>NOR A DEFINITIVE DIAGNOSIS</b>, but a indicator which can be used by a doctor to narrow down the patient's possible disease/s based on symptoms.</p>
         <p style={{ color: 'gray', fontStyle: 'italic', fontSize: '0.85em' }}>*Please read the terms and conditions before proceeding.*</p>
         <MDBBtn onClick={toggleDisclaimerModal}>Read Terms and Conditions</MDBBtn>
         <TermsAndConditions disclaimerModal={disclaimerModal} setDisclaimerModal={setDisclaimerModal} toggleDisclaimerModal={toggleDisclaimerModal} />
