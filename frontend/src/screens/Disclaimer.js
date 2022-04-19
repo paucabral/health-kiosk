40d0ab7be29588fb047e8fac28776c4cfe3a7ef5
paucabral <josepaucabral@gmail.com>
2022-04-19 14:44:07 +0800

    feat: fixed home page view

--- frontend/src/screens/Disclaimer.js
+++ frontend/src/screens/Disclaimer.js
@@ -9,8 +9,15 @@ const Disclaimer = () => {
   return (
     <React.Fragment>
       <MDBContainer>
-        <h1>Health Kiosk</h1>
-        <p className='px-5' style={{ textAlign: 'center' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue nulla id lacinia blandit. Curabitur luctus est vitae semper fringilla. Proin dignissim ante eget orci aliquam, eget feugiat magna vestibulum. In at mauris nec purus commodo dignissim vitae et turpis. Vivamus venenatis dignissim ullamcorper. Aenean ut nibh ut nibh aliquam ultrices in vel quam. Pellentesque non ligula nec risus viverra tristique. Donec pretium lacus tempus orci cursus, non blandit purus tempus.</p>
+        <MDBContainer className='mb-2'>
+          <span style={{ fontSize: '0.75em' }}>BROUGHT TO YOU BY</span>
+          <h2 style={{ fontSize: '1.25em', color: 'rgb(10, 165, 165)' }}>HIGH GROUNDS</h2>
+        </MDBContainer>
+        <MDBContainer className='mb-4'>
+          <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold', fontSize: '3em' }}>Health Kiosk</h1>
+        </MDBContainer>
+        <p className='px-5' style={{ textAlign: 'center' }}><b>Disclaimer: </b>This kiosk was designed to aid the decision making of the patient and lead them proper channels for further consultation and analysis. The results are <b>NOT FINAL</b> <b>NOR A DEFINITIVE DIAGNOSIS</b>, but a indicator which can be used by a doctor to narrow down a patient's possible disease/s based on symptoms.</p>
+        <p style={{ color: 'gray', fontStyle: 'italic', fontSize: '0.85em' }}>*Please read the terms and conditions before proceeding.*</p>
         <MDBBtn onClick={toggleDisclaimerModal}>Read Terms and Conditions</MDBBtn>
         <TermsAndConditions disclaimerModal={disclaimerModal} setDisclaimerModal={setDisclaimerModal} toggleDisclaimerModal={toggleDisclaimerModal} />
       </MDBContainer>
