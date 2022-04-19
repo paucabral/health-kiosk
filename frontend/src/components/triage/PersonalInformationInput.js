ca7d6031f8b6f9ef2b808ab38503446551374edf
paucabral <josepaucabral@gmail.com>
2022-04-19 15:12:43 +0800

    feat: label all personal information details as required

--- frontend/src/components/triage/PersonalInformationInput.js
+++ frontend/src/components/triage/PersonalInformationInput.js
@@ -29,10 +29,10 @@ const PersonalInformationInput = ({ formData, setFormData, setBtnDisable }) => {
       <MDBCardBody>
         <MDBRow className='mb-4'>
           <MDBCol>
-            <MDBInput required value={formData.first_name} onChange={(event) => setFormData({ ...formData, first_name: event.target.value.toUpperCase() })} id='first_name' size='md' label='First Name' type='text' icon="user" />
+            <MDBInput required value={formData.first_name} onChange={(event) => setFormData({ ...formData, first_name: event.target.value.toUpperCase() })} id='first_name' size='md' label='First Name * ' type='text' icon="user" />
           </MDBCol>
           <MDBCol>
-            <MDBInput required value={formData.last_name} onChange={(event) => setFormData({ ...formData, last_name: event.target.value.toUpperCase() })} id='last_name' size='md' label='Last Name' type='text' icon="user" />
+            <MDBInput required value={formData.last_name} onChange={(event) => setFormData({ ...formData, last_name: event.target.value.toUpperCase() })} id='last_name' size='md' label='Last Name * ' type='text' icon="user" />
           </MDBCol>
         </MDBRow>
         <MDBRow className='mb-2'>
@@ -44,14 +44,14 @@ const PersonalInformationInput = ({ formData, setFormData, setBtnDisable }) => {
             </MuiPickersUtilsProvider>
           </MDBCol>
           <MDBCol className='col-2'>
-            <select className='form-select' required onChange={(event) => setFormData({ ...formData, sex: event.target.value.toUpperCase() })} id="sex" label="Sex" value={formData.sex}>
-              <option disabled default value={""}>Sex</option>
+            <select className='form-select' required onChange={(event) => setFormData({ ...formData, sex: event.target.value.toUpperCase() })} id="sex" label="Sex * " value={formData.sex}>
+              <option disabled default value={""}>Sex *</option>
               <option value={"MALE"}>MALE</option>
               <option value={"FEMALE"}>FEMALE</option>
             </select>
           </MDBCol>
           <MDBCol>
-            <MDBInput required value={formData.contact_no} onChange={(event) => setFormData({ ...formData, contact_no: event.target.value })} id='contact_no' size='md' label='Contact No.' type='tel' icon="phone" />
+            <MDBInput required value={formData.contact_no} onChange={(event) => setFormData({ ...formData, contact_no: event.target.value })} id='contact_no' size='md' label='Contact No. *&nbsp;' type='tel' icon="phone" />
           </MDBCol>
         </MDBRow>
       </MDBCardBody>
