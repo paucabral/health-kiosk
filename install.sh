4f878a0da4110f724b48f9618223b8f0fff32c70
paucabral <josepaucabral@gmail.com>
2022-04-23 00:32:26 +0800

    feat: add libpq-dev under dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
 sudo apt install -y python3 python3-pip python3-venv nodejs npm postgresql
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install virtualenv
