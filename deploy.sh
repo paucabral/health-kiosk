e29358900b1cfc9369240152616b7e6a2a0c2c96
paucabral <josepaucabral@gmail.com>
2022-04-24 11:16:20 +0800

    feat: add deploy script content for frontend and update install script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd frontend/
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+fi
+npm run build
+serve -l 3000 -s build
--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y python3 python3-pip python3-venv postgresql
 sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
+sudo npm install -g serve
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install virtualenv
