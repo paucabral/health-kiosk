b410f30b2e02783e9f0af927365125ed27abfa4f
paucabral <josepaucabral@gmail.com>
2022-04-24 09:48:03 +0800

    feat: modify env and bash install

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install -y python3 python3-pip python3-venv nodejs npm postgresql
+sudo apt install -y python3 python3-pip python3-venv postgresql
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install virtualenv
