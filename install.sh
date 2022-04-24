59004db62c34c9397ed520a297dd52f24d140bb4
paucabral <josepaucabral@gmail.com>
2022-04-24 09:17:26 +0800

    feat: add shebang on the install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update -y
 sudo apt install -y python3 python3-pip python3-venv nodejs npm postgresql
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
