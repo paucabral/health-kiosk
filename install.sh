da309fc70b8330831f01c78f5cbc5023ee5c36c2
paucabral <josepaucabral@gmail.com>
2022-04-24 10:22:30 +0800

    fix: update install dependencies on node

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install -y python3 python3-pip python3-venv postgresql
 sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get update -y
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
 sudo python3 -m pip install --upgrade pip
 sudo pip3 install virtualenv
