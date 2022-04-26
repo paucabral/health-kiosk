2d91dff4da604a685b7d751f2f12c6db7be0daf5
paucabral <josepaucabral@gmail.com>
2022-04-26 11:23:31 +0800

    feat: add curl on install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y python3 python3-pip python3-venv postgresql
+sudo apt-get install -y curl python3 python3-pip python3-venv postgresql
 sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libpq-dev
