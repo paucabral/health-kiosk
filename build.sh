c3b1ea9e84d382268fa265ffa2587a3012c3c756
paucabral <josepaucabral@gmail.com>
2022-04-28 00:40:15 +0800

    feat: updated build script and add apache conf

--- build.sh
+++ build.sh
@@ -11,8 +11,20 @@ fi
 export DEBUG=False
 export ENVIRONMENT=production
 export DB_ENVIRONMENT=production
+
+sudo mkdir -p build/django/
+sudo mkdir -p build/site/
+sudo mkdir -p build/site/logs/
+sudo mkdir -p build/site/public/
+sudo mkdir -p build/site/public/static/
+
+cd build/
 [ ! -d "venv/" ] & python3 -m venv venv
-source venv/bin/activate
+
+cd /health-kiosk/django/
+
+sudo cp -R /home/pi/health-kiosk/backend/* . || sudo cp -R ~/health-kiosk/backend/* .
+
 sudo su postgres <<EOF
 createdb $DB_NAME
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
@@ -20,13 +32,19 @@ psql -c "ALTER USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
 psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
 echo "Postgres User '$DB_USER' and database '$DB_NAME' created."
 EOF
+
+source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuserwithpassword --username $DEFAULT_ADMIN_USER --password $DEFAULT_ADMIN_PASSWORD --email $DEFAULT_ADMIN_EMAIL --preserve
 
-cd ..
+sudo cp apache.conf /etc/apache2/sites-available/000-default.conf
+sudo systemctl restart apache2
+sudo systemctl enable apache2
+
+cd /home/pi/health-kiosk/ || cd ~/health-kiosk/
 
 echo "Building frontend server..."
 cd frontend/
