8510101a3763ccea8b439f00c6b655db67089fed
paucabral <josepaucabral@gmail.com>
2022-04-29 22:25:39 +0800

    feat: add contact number on admin profiles and instantiate appointment section

--- backend/management/models.py
+++ backend/management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from api.models import Patient
 
 # Create your models here.
 
@@ -9,6 +10,20 @@ class Profile(models.Model):
     profile_pic = models.ImageField(null=True, blank=True)
     affiliation = models.CharField(max_length=200, null=True, blank=True)
     position = models.CharField(max_length=200, null=True, blank=True)
+    contact_no = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
+
+class Appointment(models.Model):
+    APPOINTMENT_STATUS = (
+        ('COMPLETE', 'COMPLETE'),
+        ('PENDING', 'PENDING'),
+    )
+    patient = models.ForeignKey(Patient, null=True, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
+    appointment_status = models.CharField(
+        max_length=200, null=True, choices=APPOINTMENT_STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
