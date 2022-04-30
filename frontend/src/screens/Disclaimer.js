d709c9bfa4d22b86e838ec04a3f1188cf52457bd
paucabral <josepaucabral@gmail.com>
2022-05-01 01:52:40 +0800

    feat: add translation to disclaimer

--- frontend/src/screens/Disclaimer.js
+++ frontend/src/screens/Disclaimer.js
@@ -12,15 +12,31 @@ const Disclaimer = () => {
     <React.Fragment>
       <MDBContainer>
         <MDBContainer className='mb-2'>
-          <span style={{ fontSize: '0.75em' }}>BROUGHT TO YOU BY</span>
+          <span style={{ fontSize: '0.75em' }}>{language === "PH" ? "MULA SA PANGKAT NA" : "BROUGHT TO YOU BY"}</span>
           <h2 style={{ fontSize: '1.25em', color: 'rgb(10, 165, 165)' }}>HIGH GROUNDS</h2>
         </MDBContainer>
         <MDBContainer className='mb-4'>
           <h1 style={{ textTransform: 'uppercase', fontWeight: 'bold', fontSize: '3em' }}>Health Kiosk</h1>
         </MDBContainer>
-        <p className='px-5' style={{ textAlign: 'center' }}><b>Disclaimer: </b>This kiosk is designed to aid in the decision making of the patient <i>(18 years old and above)</i> and lead them proper channels for further consultation and analysis. The results are <b>NOT FINAL</b> <b>NOR A DEFINITIVE DIAGNOSIS</b>, but an indicator which can be used by the doctor to narrow down the patient's possible disease/s based on symptoms.</p>
-        <p style={{ color: 'gray', fontStyle: 'italic', fontSize: '0.85em' }}>*Please read the terms and conditions before proceeding.*</p>
-        <MDBBtn onClick={toggleDisclaimerModal}>Read Terms and Conditions</MDBBtn>
+        {language === "PH" ?
+          <div>
+            <p className='px-5' style={{ textAlign: 'center' }}><b>Paunawa: </b>Ang kiosk na ito ay idinisenyo upang tumulong sa paggawa ng desisyon ng pasyente <i>(18 taong gulang pataas)</i> at magbigay gabay sa kanila patungo sa mga tamang channel para sa karagdagang konsultasyon at pagsusuri. Ang mga resulta ay <b>HINDI PINAL</b> <b>AT HINDI RIN PANAPOS NA KONKLUSYON</b>, ngunit isang lamang gabay na maaaring gamitin ng doktor sa pagtingin ng posibleng sakit ng pasyente batay sa mga sintomas.</p>
+            <p style={{ color: 'gray', fontStyle: 'italic', fontSize: '0.85em' }}>*Mangyaring basahin ang mga tuntunin at kundisyon bago magpatuloy.*</p>
+          </div>
+          :
+          <div>
+            <p className='px-5' style={{ textAlign: 'center' }}><b>Disclaimer: </b>This kiosk is designed to aid in the decision making of the patient <i>(18 years old and above)</i> and lead them proper channels for further consultation and analysis. The results are <b>NOT FINAL</b> <b>NOR A DEFINITIVE DIAGNOSIS</b>, but an indicator which can be used by the doctor to narrow down the patient's possible disease/s based on symptoms.</p>
+            <p style={{ color: 'gray', fontStyle: 'italic', fontSize: '0.85em' }}>*Please read the terms and conditions before proceeding.*</p>
+          </div>
+        }
+
+        <MDBBtn onClick={toggleDisclaimerModal}>
+          {language === "PH" ?
+            "Basahin ang mga tuntunin at kundisyon"
+            :
+            "Read Terms and Conditions"
+          }
+        </MDBBtn>
         <TermsAndConditions disclaimerModal={disclaimerModal} setDisclaimerModal={setDisclaimerModal} toggleDisclaimerModal={toggleDisclaimerModal} />
       </MDBContainer>
     </React.Fragment>
