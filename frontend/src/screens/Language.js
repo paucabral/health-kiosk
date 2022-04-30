6618a0c2e2eeb5b5efd976b1f48540aae154a1ba
paucabral <josepaucabral@gmail.com>
2022-05-01 01:07:34 +0800

    feat: add description on buttons

--- frontend/src/screens/Language.js
+++ frontend/src/screens/Language.js
@@ -1,4 +1,4 @@
-import { MDBCol, MDBRow, MDBContainer } from 'mdb-react-ui-kit'
+import { MDBCol, MDBRow, MDBBtn, MDBIcon, MDBContainer } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const Language = () => {
@@ -7,12 +7,28 @@ const Language = () => {
       <MDBContainer>
         <h1>Select Language / Pumili ng Wika</h1>
         <MDBContainer>
-          <MDBRow>
+          <MDBRow className='py-2 px-5 mx-5'>
             <MDBCol>
-
+              <MDBBtn color='primary' style={{ width: '300px', height: '320px', borderRadius: '10%' }}>
+                <h3>
+                  <MDBIcon fas icon="globe-asia" /> English
+                </h3>
+                <br />
+                <p style={{ fontSize: '1.5em' }}>
+                  The instructions will be shown using the English language.
+                </p>
+              </MDBBtn>
             </MDBCol>
             <MDBCol>
-
+              <MDBBtn color='danger' style={{ width: '300px', height: '320px', borderRadius: '10%' }}>
+                <h3>
+                  <MDBIcon fas icon="globe-americas" /> Filipino
+                </h3>
+                <br />
+                <p style={{ fontSize: '1.5em' }}>
+                  Ang mga panuto ay ipapakita gamit ang wikang Filipino.
+                </p>
+              </MDBBtn>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
