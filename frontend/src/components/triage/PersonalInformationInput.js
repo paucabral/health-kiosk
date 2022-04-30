a000879bf46674ae346dff490022738dc8707886
paucabral <josepaucabral@gmail.com>
2022-05-01 02:07:38 +0800

    feat: add translation on personal information input

--- frontend/src/components/triage/PersonalInformationInput.js
+++ frontend/src/components/triage/PersonalInformationInput.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { MDBInput, MDBCardBody, MDBCardTitle, MDBCol, MDBRow } from 'mdb-react-ui-kit';
 import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+import { LanguageContext } from '../../contexts/LanguageContext';
 
 const defaultMaterialTheme = createTheme({
   palette: {
@@ -22,38 +23,55 @@ const PersonalInformationInput = ({ formData, setFormData, setBtnDisable }) => {
       setBtnDisable(false)
     }
   }, [formData])
+
+  const { language, setLanguage } = useContext(LanguageContext);
+
   return (
     <React.Fragment>
-      <MDBCardTitle style={{ fontWeight: "bold" }}>Personal Information</MDBCardTitle>
-      <p>Please fill up your personal information.</p>
+      <MDBCardTitle style={{ fontWeight: "bold" }}>
+        {language === "PH" ?
+          "Personal na Impormasyon"
+          :
+          "Personal Information"
+        }
+      </MDBCardTitle>
+      {language === "PH" ?
+        <p>
+          Mangyaring punan ang iyong personal na impormasyon.
+        </p>
+        :
+        <p>
+          Please fill up your personal information.
+        </p>
+      }
       <MDBCardBody>
         <MDBRow className='mb-4'>
           <MDBCol>
-            <MDBInput required value={formData.first_name} onChange={(event) => setFormData({ ...formData, first_name: event.target.value.toUpperCase() })} id='first_name' size='md' label='First Name * ' type='text' icon="user" />
+            <MDBInput required value={formData.first_name} onChange={(event) => setFormData({ ...formData, first_name: event.target.value.toUpperCase() })} id='first_name' size='md' label={language === "PH" ? 'Pangalan * ' : 'First Name * '} type='text' icon="user" />
           </MDBCol>
           <MDBCol>
-            <MDBInput required value={formData.last_name} onChange={(event) => setFormData({ ...formData, last_name: event.target.value.toUpperCase() })} id='last_name' size='md' label='Last Name * ' type='text' icon="user" />
+            <MDBInput required value={formData.last_name} onChange={(event) => setFormData({ ...formData, last_name: event.target.value.toUpperCase() })} id='last_name' size='md' label={language === "PH" ? 'Apelyido * ' : 'Last Name * '} type='text' icon="user" />
           </MDBCol>
         </MDBRow>
         <MDBRow className='mb-2'>
           <MDBCol className='col-2'>
             <MuiPickersUtilsProvider utils={MomentUtils}>
               <ThemeProvider theme={defaultMaterialTheme}>
-                <DatePicker size='small' variant='dialog' inputVariant="outlined" minDate={moment().subtract(500, "years")} maxDate={moment().subtract(18, "years")} required format="MM/DD/YYYY" label="Birthday" value={formData.birth_date} onChange={(event) => setFormData({ ...formData, birth_date: moment(event._d).format('YYYY-MM-DD') })} />
+                <DatePicker size='small' variant='dialog' inputVariant="outlined" minDate={moment().subtract(500, "years")} maxDate={moment().subtract(18, "years")} required format="MM/DD/YYYY" label={language === "PH" ? 'Kaarawan' : 'Birthday'} value={formData.birth_date} onChange={(event) => setFormData({ ...formData, birth_date: moment(event._d).format('YYYY-MM-DD') })} />
               </ThemeProvider>
             </MuiPickersUtilsProvider>
           </MDBCol>
           <MDBCol className='col-2'>
-            <select className='form-select' required onChange={(event) => setFormData({ ...formData, sex: event.target.value.toUpperCase() })} id="sex" label="Sex * " value={formData.sex}>
-              <option disabled default value={""}>Sex *</option>
-              <option value={"MALE"}>MALE</option>
-              <option value={"FEMALE"}>FEMALE</option>
+            <select className='form-select' required onChange={(event) => setFormData({ ...formData, sex: event.target.value.toUpperCase() })} id="sex" label={language === "PH" ? 'Kasarian * ' : 'Sex * '} value={formData.sex}>
+              <option disabled default value={""}>{language === "PH" ? 'Kasarian' : 'Sex'}</option>
+              <option value={"MALE"}>{language === "PH" ? 'LALAKI' : 'MALE'}</option>
+              <option value={"FEMALE"}>{language === "PH" ? 'BABAE' : 'FEMALE'}</option>
             </select>
           </MDBCol>
           <MDBCol>
             <MDBInput required pattern='[0-9]*' value={formData.contact_no} onChange={(event) => {
               setFormData({ ...formData, contact_no: event.target.value.replace(/[^0-9]+/g, '') })
-            }} id='contact_no' size='md' label='Contact No. *&nbsp;' type='tel' icon="phone" />
+            }} id='contact_no' size='md' label={language === "PH" ? 'Numero ng Telepono * ' : 'Contact No. * '} type='tel' icon="phone" />
           </MDBCol>
         </MDBRow>
       </MDBCardBody>
