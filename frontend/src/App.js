37a95b7340e5fcf643d3e010e3df101e118901c6
paucabral <josepaucabral@gmail.com>
2022-05-01 00:52:12 +0800

    feat: instantiate language selection screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Facilities from './screens/Facilities';
 import Results from './screens/Results';
 import Disclaimer from './screens/Disclaimer';
 import About from './screens/About';
+import Language from './screens/Language';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <div className='d-flex align-items-center justify-content-center' style={{ height: "100%" }}>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/" element={<Disclaimer />} />
+            <Route exact path="/" element={<Language />} />
+            <Route exact path="/disclaimer" element={<Disclaimer />} />
             <Route path="/check" element={<Triage />} />
             <Route path="/results" element={<Results />} />
             <Route path="/nearest-facilities" element={<Facilities />} />
---
+++ frontend/src/screens/Language.js
@@ -0,0 +1,24 @@
+import { MDBCol, MDBRow, MDBContainer } from 'mdb-react-ui-kit'
+import React from 'react'
+
+const Language = () => {
+  return (
+    <React.Fragment>
+      <MDBContainer>
+        <h1>Select Language / Pumili ng Wika</h1>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol>
+
+            </MDBCol>
+            <MDBCol>
+
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </MDBContainer>
+    </React.Fragment>
+  )
+}
+
+export default Language
